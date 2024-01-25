ba39df82667d5ca0e4571b2b3d588c8f9436c830
B. Chen <chenb@sunysuffolk.edu>
2024-01-25 14:07:27 -0500

    Lecture 2

---
+++ L01 Introduction/src/p1/Main.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class Main {
+	public static void main(String[] args) {
+		// how to display things
+		System.out.println("\t  Joe's Restaurant");
+		System.out.println("\t   (631)123-4567");
+		System.out.println("\n---------------------------------");
+		System.out.println("1.\tBeef\t\t$19.99");
+		System.out.println("2.\tFish\t\t$24.99");
+		System.out.println("3.\tLamb\t\t$22.99");
+		System.out.println("4.\tEggs\t\t $9.99");
+		System.out.println("\n---------------------------------");
+	}
+
+}
