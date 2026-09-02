2b17bff76005ccf7ea7e5704ff543ab16164869f
B. Chen <chenb@sunysuffolk.edu>
2026-09-02 11:13:48 -0400

    Lecture 1

---
+++ MW L01 Basic Java/src/p1/HelloWorld.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		String hello = "Hi";// declare a variable called hello holding a String value 'Hi"
+		System.out.println(hello);
+		System.out.println("1" + "2");
+		int x = 3;
+		int y = 2;
+		System.out.println((double) x / y); // casting
+		System.out.println(3 - 2);
+		System.out.println(3 * 2);
+		System.out.println(1 % 3); // modulus
+	}
+}
